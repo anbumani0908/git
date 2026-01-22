ccb975d90e02d1c78804667024ed90b7633fbcf3
demo <anbumanivenkatesan0908@gmail.com>
2026-01-22 15:02:41 +0530

    Anbu

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Welcome to the git course!');
+console.log('Welcome to the git course! and this is the index.js file.');
+for(var i = 0; i < 5; i++) {{
+    console.log('anbumani');
+}
+}
